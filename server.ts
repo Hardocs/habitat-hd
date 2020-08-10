2604342e8e01cf08865df6f9f8376513b2fe03cd
Narration SD <narrationsd@gmail.com>
2020-08-09 17:36:13 -0700

    Initial commit - alligator dummy server

---
+++ server.ts
@@ -0,0 +1,8 @@
+import express = require ('express')
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('An alligator approaches!');
+});
+
+app.listen(3000, () => console.log('Gator app listening on port 3000!'));
