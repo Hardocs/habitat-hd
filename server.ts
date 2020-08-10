4ddc032b82382d46328bd576f12ee810623723b0
Narration SD <narrationsd@gmail.com>
2020-08-10 10:17:33 -0700

    first stage: proxy all. probably only the * route is needed

--- server.ts
+++ server.ts
@@ -3,8 +3,22 @@ const app = express()
 
 app.get('/', (req, res) => {
   console.log ('req headers:' + JSON.stringify(req.headers))
-  res.send('An alligator approaches!' +
-    '  headers: ' + JSON.stringify(req.headers))
+  res.send('{ "alligator": "approaches /", "url: ": "' + req.url + '", "headers": ' + JSON.stringify(req.headers) + '}')
 })
 
-app.listen(3000, () => console.log('Gator app listening on port 3000!'))
+app.get('*', (req, res) => {
+  console.log ('req headers:' + JSON.stringify(req.headers))
+  res.send('{ "alligator": "approaches .*", "url: ": "' + req.url + '", "headers": ' + JSON.stringify(req.headers) + '}')
+})
+
+app.get('/oauth2/', (req, res) => {
+  console.log ('/oauth2/ req headers:' + JSON.stringify(req.headers))
+  res.send('{ "alligator": "approaches /oauth2/", "url: ": "' + req.url + '", "headers": ' + JSON.stringify(req.headers) + '}')
+})
+
+app.get('/oauth2', (req, res) => {
+  console.log ('/oauth2 req headers:' + JSON.stringify(req.headers))
+  res.send('{ "alligator": "approaches /oauth2", "url: ": "' + req.url + '", "headers": ' + JSON.stringify(req.headers) + '}')
+})
+
+app.listen(5984, () => console.log('Gator app listening on port 5984!'))
