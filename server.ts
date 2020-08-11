25ddca59a8467a4eace701e047d6482aaa609fa4
Narration SD <narrationsd@gmail.com>
2020-08-10 19:33:05 -0700

    This works as full proxy to allow us to clear the prefix, on Linux oauth2-proxy stack. With one exception so far: for /_utils, a trailing slash is required.

--- server.ts
+++ server.ts
@@ -3,16 +3,20 @@ import express = require ('express')
 import { createProxyMiddleware, Filter, Options, RequestHandler } from 'http-proxy-middleware';
 
 const app = express()
-const listenPort = 5983 // distinguish from 5984 so we can talk to it...
-const proxyDestination = 'https://nsd.narrationsd.com'
-// const proxyDestination = 'localhost:5984'
+const listenPort:number = 5983 // distinguish from 5984 so we can talk to it...
+const proxyDestination = 'http://127.0.0.1:5984'
+
+// the REs look a little funny, so that empty queries will act
+// sensibly regardless of trailing slash presence or not
 
 app.use('/oauth2', createProxyMiddleware({
   target: proxyDestination,
-  pathRewrite: {'^/oauth2/?' : ''},
-  changeOrigin: true }));
+  pathRewrite: {'/oauth2/?' : ''},
+  logLevel: 'debug',
+  changeOrigin: false }));
 
 app.get('*', (req, res) => {
+  console.log ('req url:' + JSON.stringify(req.url))
   console.log ('req headers:' + JSON.stringify(req.headers))
   res.send('{ "alligator": "approaches .*", "url: ": "' + req.url + '", "headers": ' + JSON.stringify(req.headers) + '}')
 })
