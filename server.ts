b29e53a8ef923cf6bb0bb01e1eefb961902e6343
Narration SD <narrationsd@gmail.com>
2020-08-26 17:10:10 -0700

    we are up with fundamental secured database. Gmail ids are required, and then we only allow a couple of those. Admin is provided for, not as nicely as wished, as CloudDb doesn't support this through proxy auth, or seemingly allow around it. A separate proxy may be indicated, but let's see. You can log in now to use Fauxton, so not critical indeed for first Hardocs. Db of owners to come soon. Access is now hard-api.

--- server.ts
+++ server.ts
@@ -1,25 +1,93 @@
 import express = require ('express')
 // import * as express from 'express';
-// also this - do we have use for the extra three? leave the ide warning in commit to remind
+import crypto from 'crypto'
+// also this - do we have use for the extra three? leave the ide warning in commit to remindnp
 import { createProxyMiddleware, Filter, Options, RequestHandler } from 'http-proxy-middleware';
 
 const app = express()
 const listenPort:number = 5983 // distinguish from 5984 so we can talk to it...
-const proxyDestination:string = 'http://127.0.0.1:5984'
+let proxyDestination:string = 'http://localhost:5984'
+
+app.use('/hard-api', async (req, res, next) => {
+
+  console.log ('req url:' + JSON.stringify(req.url))
+  // console.log ('req headers:' + JSON.stringify(req.headers))
+  console.log('req email: ' + req.headers['x-forwarded-email'])
+
+  // later we look these up in db, separate connection
+  const reqEmail = req.headers['x-forwarded-email']
+  let dbId = ''
+  let dbRoles = 'users'
+  let authType = 'proxy'
+
+  switch (reqEmail) {
+    case'narrationsd@gmail.com':
+      dbId = 'admin-hard'
+      dbRoles = 'admins'
+      // authType = 'basic' // see note for later
+      break
+    case 'narrishin@gmail.com':
+      dbId = 'hardlab-gm'
+      dbRoles = 'users'
+      break
+    default:
+      console.log('bad destination: ' + reqEmail)
+      dbId = 'not-permitted'
+  }
+
+  console.log('req id is: ' + dbId)
+
+  if (authType === 'proxy') {
+    // here's the key
+    req.headers['x-auth-couchdb-username'] = dbId
+    req.headers['x-auth-couchdb-roles'] = dbRoles
+
+    // temporary - later an env, matching couchdb/etc/local.ini value
+    const secret = 'be55d146bea2f6d2f7c597b67210e337'
+
+    // as required by Couch
+    const hmac = crypto.createHmac('sha1', secret)
+    hmac.update(dbId)
+    const token:string = hmac.digest('hex')
+
+    console.log('token is: ' + token)
+    req.headers["x-auth-couchdb-token"] = token
+  }
+  else {  // *todo* this didn't work - can't bypass our friend, apparently. But later, change the proxy request w/name:pw?
+
+    const authString:string = 'admin-hard:4redwood' // *todo* obviously another env
+    const basicAuth:string = new Buffer(authString).toString('base64')
+
+    req.headers['Authorization'] = 'Basic ' + basicAuth
+    req.headers['x-auth-couchdb-username'] = dbId
+    req.headers['x-auth-couchdb-roles'] = dbRoles
+  }
+  next()
+});
+
+// this one will go out later, or in some way use a debug key,  because that's what it is
+app.use('/hard-api', async (req, res, next) => {
+
+  console.log ('checking req url:' + JSON.stringify(req.url))
+  console.log ('checking req id:' + JSON.stringify(req.headers['x-auth-couchdb-username']))
+  console.log ('checking req token:' + JSON.stringify(req.headers["x-auth-couchdb-token"]))
+  console.log ('checking req roles:' + JSON.stringify(req.headers["x-auth-couchdb-roles"]))
+  // console.log ('checking req Authorization:' + JSON.stringify(req.headers["Authorization"]))
+  // console.log ('checking req headers:' + JSON.stringify(req.headers))
+  next()
+});
 
 // the REs look a little funny, so that empty queries will act
 // sensibly regardless of trailing slash presence or not
 
-app.use('/oauth2', createProxyMiddleware({
+app.use('/hard-api', createProxyMiddleware({
   target: proxyDestination,
-  pathRewrite: {'/oauth2/?' : ''},
+  pathRewrite: {'/hard-api/?' : ''},
   logLevel: 'debug',
-  changeOrigin: false }));
+  changeOrigin: true
+}));
 
-app.get('*', (req, res) => {
-  console.log ('req url:' + JSON.stringify(req.url))
-  console.log ('req headers:' + JSON.stringify(req.headers))
-  res.send('{ "alligator": "approaches .*", "url: ": "' + req.url + '", "headers": ' + JSON.stringify(req.headers) + '}')
-})
-
-app.listen(listenPort, () => console.log('Gator app listening on port ' + listenPort + '!'))
+app.listen(listenPort,
+    () => {
+      console.log('Gator app listening on port ' + listenPort + '!')
+    })
