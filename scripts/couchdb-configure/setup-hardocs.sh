0909601c721f728169d06fb2e0de7a218b1d789c
Narration SD <narrationsd@gmail.com>
2020-09-18 01:28:47 -0700

    two views and more queries

--- scripts/couchdb-configure/setup-hardocs.sh
+++ scripts/couchdb-configure/setup-hardocs.sh
@@ -2,13 +2,17 @@
 # then create the hardocs projects database
 curl -X PUT $dbs/hardocs-projects
 
-# create basis view
+# create design doc withh two view
 
 {
   "_id": "_design/projects",
+  "_rev": "2-b33ad030c1d8819228056332de560688",
   "views": {
     "owner-projects": {
       "map": "function (doc) {\n  if (doc)\n  emit(doc.owner, 1);\n}"
+    },
+    "owner-project": {
+      "map": "function (doc) {\n  if (doc)\n  emit(doc.owner + ':' + doc.project, 1);\n}"
     }
   },
   "language": "javascript"
@@ -23,3 +27,6 @@ curl $dbs/hardocs-projects/_design/projects/_view/owner-projects/?include_docs=t
 # selected by view
 curl "$dbs/hardocs-projects/_design/projects/_view/owner-projects?key=%22narreshen%22&include_docs=true"
 curl "$dbs/hardocs-projects/_design/projects/_view/owner-projects?include_docs=true&key=%22narreshen%22"
+
+# a single project, with our second view
+ curl "$dbs/hardocs-projects/_design/projects/_view/owner-project?key=%22narreshen:proj-2%22&wut=wow&include_docs=true"
