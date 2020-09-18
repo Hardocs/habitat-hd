9afa1bed07c8670961e883311ed9ba8a298dc282
Narration SD <narrationsd@gmail.com>
2020-09-18 01:08:31 -0700

    first view and queries for it

--- scripts/couchdb-configure/setup-hardocs.sh
+++ scripts/couchdb-configure/setup-hardocs.sh
@@ -4,3 +4,22 @@ curl -X PUT $dbs/hardocs-projects
 
 # create basis view
 
+{
+  "_id": "_design/projects",
+  "views": {
+    "owner-projects": {
+      "map": "function (doc) {\n  if (doc)\n  emit(doc.owner, 1);\n}"
+    }
+  },
+  "language": "javascript"
+}
+
+# all docs
+ curl $dbs/hardocs-projects/_all_docs?include_docs=true
+
+# all by view
+curl $dbs/hardocs-projects/_design/projects/_view/owner-projects/?include_docs=true
+
+# selected by view
+curl "$dbs/hardocs-projects/_design/projects/_view/owner-projects?key=%22narreshen%22&include_docs=true"
+curl "$dbs/hardocs-projects/_design/projects/_view/owner-projects?include_docs=true&key=%22narreshen%22"
